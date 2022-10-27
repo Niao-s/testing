c9ac68ba7774ccce3ac4aeed684442c7d8ba4d40
OSokolova <oks.sokolova.7@gmail.com>
2022-10-27 10:47:52 +0300

    no message

--- src/client/modules/my/lookup/lookup.js
+++ src/client/modules/my/lookup/lookup.js
@@ -1,6 +1,7 @@
 import {LightningElement} from 'lwc';
 
 export default class Lookup extends LightningElement {
+    _handler;
     lookupResult = '';
     lookupArray = [
         {
@@ -21,14 +22,36 @@ export default class Lookup extends LightningElement {
         this.template.appendChild(styles);
     }
 
+    connectedCallback() {
+        document.addEventListener('click', this._handler = this.close.bind(this));
+        window.onmessage = (event) => {
+            let message = event.data ? JSON.parse(event.data) : {};
+            if (message && message.command) {
+                let command = message.command;
+                console.log(command);
+            }
+        }
+    }
+
+    disconnectedCallback() {
+        document.removeEventListener('click', this._handler);
+    }
+
+    close() {
+        this.lookupArray = undefined;
+    }
+
     handleSearchLookup = (evt) => {
         let inputValue = evt.target.value;
-        console.log(evt.target.value);
+        console.log(inputValue);
+        this.sendMsgToParent(JSON.stringify({ command: "GetLookupData", searchStr: inputValue }));
     }
 
     startClickLookupEvt = (evt) => {
+        evt.stopPropagation();
         let currentIdx = evt.currentTarget.dataset.idx;
         let currentVal = evt.currentTarget.dataset.val;
+        this.lookupResult = currentVal;
         const action = new CustomEvent('vallicked', {
             detail: {
                 currentIdx: currentIdx,
@@ -40,5 +63,10 @@ export default class Lookup extends LightningElement {
 
         this.dispatchEvent(action);
         this.lookupArray = undefined;
+        return false;
+    }
+
+    sendMsgToParent = (message) => {
+        window.parent.postMessage((message), '*');
     }
 }
