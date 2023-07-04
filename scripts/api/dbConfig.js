84dda1ffec3340faad692b3b2b6e033851c95334
OSokolova <oks.sokolova.7@gmail.com>
2023-07-04 09:55:31 +0300

    no message

--- scripts/api/dbConfig.js
+++ scripts/api/dbConfig.js
@@ -5,7 +5,12 @@ const pool = new Pool({
     host: 'dpg-cihrjjd9aq012evopd00-a.oregon-postgres.render.com',
     database: 'zp_test_base',
     password: 'iEPoXXHP3TOGd0Znd144nYwU4XZJo0Sz',
-    port: 5432
+    port: 5432,
+    ssl: true
+});
+
+pool.on('error', function (err, client) {
+    console.error('idle client error', err.message, err.stack)
 })
 
 module.exports = pool;
