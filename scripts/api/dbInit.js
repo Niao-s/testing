9a1f8e299412306e76443b0f66bd0a93d9190c1e
OSokolova <oks.sokolova.7@gmail.com>
2023-07-04 10:27:28 +0300

    no message

--- scripts/api/dbInit.js
+++ scripts/api/dbInit.js
@@ -5,7 +5,7 @@ const dbInit = () => {
     pool.query(`
         CREATE TABLE IF NOT EXISTS request_schema (
             id SERIAL NOT NULL PRIMARY KEY,
-            json_structure VARCHAR(1000),
+            json_structure VARCHAR(5000),
             created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
             );`, (error, results) => {
         if (error) {
@@ -15,7 +15,7 @@ const dbInit = () => {
 
     // Add new columns
     pool.query(`
-    ALTER TABLE IF EXISTS request_schema ADD COLUMN IF NOT EXISTS json_structure VARCHAR(1000);    
+    ALTER TABLE IF EXISTS request_schema ADD COLUMN IF NOT EXISTS json_structure VARCHAR(5000);    
     `, (error, results) => {
         if (error) {
             console.log(error);
