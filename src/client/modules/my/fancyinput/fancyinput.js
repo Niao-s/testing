62429409d1418860b82bc9cb4d8107dbfd4fb8ff
OSokolova <oks.sokolova.7@gmail.com>
2023-09-05 21:25:17 +0300

    no message

--- src/client/modules/my/fancyinput/fancyinput.js
+++ src/client/modules/my/fancyinput/fancyinput.js
@@ -2,8 +2,13 @@ import {LightningElement} from "lwc";
 
 export default class fancyinput extends LightningElement {
 
-    constructor() {
+    simpledText = '';
+    fancyText = '';
 
+    openedDivStyle = '<code class="parsedtext">{{';
+    closedDivStyle = '}}</code>';
+
+    constructor() {
         super();
         const styles = document.createElement('link');
         styles.href = './resources/css/bootstrap.min.css';
@@ -13,5 +18,25 @@ export default class fancyinput extends LightningElement {
 
     connectedCallback() {
         console.log('test');
+    }
+
+    renderedCallback() {
+        this.setFancedInput();
+    }
+
+    setFancedInput = () => {
+        this.simpledText = '{\n' +
+            '    "Name": "Account",\n' +
+            '    "Id": "{{1.response.Entity.GoLegalEntityId}}",\n' +
+            '    "fields": [\n' +
+            '        {\n' +
+            '            "fieldName": "GoTaxSystemId",\n' +
+            '            "fieldValue": "{{request.tax_system}}"\n' +
+            '        }\n' +
+            '    ]\n' +
+            '}';
+        this.fancyText = this.simpledText.replaceAll('{{', this.openedDivStyle).replaceAll('}}', this.closedDivStyle);
+        console.log(this.fancyText);
+        this.template.querySelector('[data-id="faketexstarea"]').innerHTML = this.fancyText;
     }
 }
