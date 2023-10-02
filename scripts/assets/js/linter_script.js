6ab56f23865166cc4cccba7c599fea902038229d
OSokolova <oks.sokolova.7@gmail.com>
2023-10-02 11:38:10 +0300

    no message

--- scripts/assets/js/linter_script.js
+++ scripts/assets/js/linter_script.js
@@ -23,15 +23,24 @@ document.addEventListener("DOMContentLoaded", function() {
 function validateJson () {
     try{
         let json = codeEditor.getValue();
-        console.log(json);
         let jsonStr = json.toString();
-        console.log(jsonStr);
         jsonlint.parse(jsonStr);
         showErrorElementSuccess();
     }
     catch (err) {
         let lineNumber = err;
-        console.log(lineNumber);
+        console.log(err);
+        let regex = /[+-]?\d+(?:\.\d+)?/g;
+        let str = lineNumber.toString();
+        let lin;
+        let match;
+        while (match = regex.exec(str)) {
+            lin=match[0];
+            break;
+        }
+        let errorElem = document.querySelectorAll(".CodeMirror-line")[lin-1];
+        errorElem.style["background-color"] = "#f8d7da";
+        showErrorElementError(err);
     }
 }
 
@@ -55,52 +64,9 @@ function showErrorElementSuccess () {
     errorElement.innerText = "Valid Json";
 }
 
-$(document).ready(function(){
-    var code=$(".codemirror-textarea")[0];
-
-    var editor=CodeMirror.fromTextArea(code,{
-        lineNumbers: true,
-        lineWrapping: true,
-        styleActiveLine: true,
-        styleActiveSelected: true,
-        mode: 'application/json',
-        gutters: ["CodeMirror-lint-markers"],
-        lint:true
-
-    });
-
-    $json="";
-    $('#validate').click(function(){
-        try {
-        $json=editor.getValue();
-        {
-            $djson=$json.toString();
-            $djson=jsonlint.parse($djson);
-            $('#error').show();
-            $('#error').removeClass("alert-danger");
-            $('#error').addClass("alert-success");
-            $("#error").empty().append("Valid Json"); 
-        }       
-        }
-        catch(err) {
-            var lineNumber=err;
-            var regex = /[+-]?\d+(?:\.\d+)?/g;
-            var str = lineNumber.toString();
-            var lin;
-            var match;
-             while (match = regex.exec(str)) {
-                 lin=match[0];
-                    break;
-                }
-                $( ".CodeMirror-line:eq("+(lin-1)+")" ).css( "background-color", "#f8d7da");
-                $('#error').show();
-                $('#error').removeClass("alert-success");
-                $('#error').addClass("alert-danger");
-                $("#error").empty().append(err);
-            }
-    })
-    $('#reset').click(function(){
-        editor.setValue('');
-        $('#error').hide();
-    });
-})
+function showErrorElementError (text) {
+    showErrorElement();
+    errorElement.classList.remove("alert-success");
+    errorElement.classList.add("alert-danger");
+    errorElement.innerText = text;
+}
